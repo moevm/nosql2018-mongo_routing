c9059331f4bf0a8c46682946bddb34800aea7aa1
Nikita Shabashov <nikita_shabashov@epam.com>
2018-09-12 22:29:58 +0300

    Init project

---
+++ src/main/java/ru/zmaps/controllers/GreetingController.java
@@ -0,0 +1,14 @@
+package ru.zmaps.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String greeting() {
+        return "greeting";
+    }
+
+}
