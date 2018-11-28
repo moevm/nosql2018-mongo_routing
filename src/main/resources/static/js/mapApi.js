6a74cef345f76a09d85b12b3bb0f4e4e3a1927f7
MariyaF1998 <mask-1998@mail.ru>
2018-11-28 21:11:06 +0300

    Add functions of drawing Polyline and Polygon

--- src/main/resources/static/js/mapApi.js
+++ src/main/resources/static/js/mapApi.js
@@ -42,4 +42,40 @@ map.on('click', function(e) {
         .openOn(map)
 })
 
+function drawLine(coordinates,color,weight,opacity,smooth){
+let polyline = L.polyline(coordinates, 
+{color: color, 
+weight: weight, 
+opacity: opacity, 
+smoothFactor: smooth}).addTo(map); 
+map.fitBounds(polyline.getBounds());
+return polyline;
+}
 
+drawLine([[59.991318, 30.319007],[59.994173, 30.316496],[59.977697, 30.317652], [59.972065, 30.307578]],'red',3, 0.5, 1);
+
+function drawPolygon(coordinates,color,weight,opacity,smooth){
+let polygon = L.polygon(coordinates, 
+{color: color, 
+weight: weight, 
+opacity: opacity, 
+smoothFactor: smooth}).addTo(map); 
+map.fitBounds(polygon.getBounds());
+return polygon;
+}
+
+drawPolygon([[59.969732, 30.301888],[59.967111, 30.309849],[59.964157, 30.308540], [59.959576, 30.301448]],'blue',5, 0.5, 1);
+
+//var polyline = L.polyline([[59.991318, 30.319007],[59.994173, 30.316496],[59.977697, 30.317652], [59.972065, 30.307578]],
+//{color: 'red',
+//weight: 3,
+//opacity: 0.5,
+//smoothFactor: 1}).addTo(map);
+
+
+//var polygon = L.polygon([[59.991318, 30.319007],[59.994173, 30.316496],[59.977697, 30.317652], [59.972065, 30.307578], [59.955436, 30.294366]],
+//{color: 'blue',
+//weight: 4 ,
+//opacity: 0.5,
+//smoothFactor: 1}).addTo(map);
+//map.fitBounds(polygon.getBounds());
